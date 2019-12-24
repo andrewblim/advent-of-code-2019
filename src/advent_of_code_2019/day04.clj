95c175db4cc2f1fccf50a005d4cec8347db31213
Andrew Lim <andy@amperity.com>
2019-12-24 07:21:50 -0500

    Fix day04 file structure

--- src/advent_of_code_2019/day04.clj
+++ src/advent_of_code_2019/day04.clj
@@ -11,29 +11,57 @@
         (map (fn [[x y]] (- x (* base y))))
         (reverse))))
 
-(split-into-digits 1234567)
-
 (defn no-decreasing-digits
+  "Does the sequence of digits have any decreasing?"
   [digs]
   (every? (fn [[x y]] (<= x y)) (partition 2 1 digs)))
 
-(no-decreasing-digits [1 2 1])
-
 (defn has-double-digits
+  "Does the sequence of digits have any consecutive repeats?"
   [digs]
   (some (fn [[x y]] (= x y)) (partition 2 1 digs)))
 
 (defn number-ok?
+  "Is the number OK as per no-decreasing-digits and has-double-digits?"
   [x]
   (let [digs (split-into-digits x)]
     (and (no-decreasing-digits digs)
          (has-double-digits digs))))
 
-(number-ok? 111111)
-(number-ok? 223450)
-(number-ok? 123789)
+(defn answer-question-1
+  []
+  (->> (range 125730 (inc 579381))
+       (filter number-ok?)
+       (count)))
 
-(->> (range 125730 (inc 579381))
-     (filter number-ok?)
-     (count))
+;; (answer-question-1)
 
+;; Question2
+
+(defn lengths-of-runs-of-digits
+  "Get the lengths of runs of consecutive digits"
+  [digs]
+  (->> digs
+       (partition-by identity)
+       (map count)))
+
+(defn has-run-of-length?
+  "Does the sequence of digits have at least one with some length (exactly)?"
+  [len digs]
+  (->> (lengths-of-runs-of-digits digs)
+       (some #(= % len))))
+
+(defn number-ok-2?
+  "Is the number OK as per revised criteria in question 2?"
+  [x]
+  (let [digs (split-into-digits x)]
+    (and (no-decreasing-digits digs)
+         (has-run-of-length? 2 digs))))
+
+(defn answer-question-2
+  []
+  (->> (range 125730 (inc 579381))
+       (filter number-ok-2?)
+       (count)))
+
+;; (answer-question-2)
