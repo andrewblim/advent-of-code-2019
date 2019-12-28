2227f9e7c720ed132c3c46498216512ececdbb73
Andrew Lim <andy@amperity.com>
2019-12-27 21:27:35 -0500

    day07 part 2

--- src/advent_of_code_2019/day07.clj
+++ src/advent_of_code_2019/day07.clj
@@ -24,82 +24,112 @@
       1 x
       (throw (Exception. (str "Unrecognized mode " mode))))))
 
-(defn process-intcode
-  "Process the operations in an intcode, returning the updated intcode.
-  Input and output should be supplied as lists"
-  ([intcode input] (process-intcode intcode 0 input '()))
-  ([intcode offset input output]
-   (let [[op mode1 mode2 mode3] (split-opcode (get intcode offset))]
-     (case op
-       1 (let [res (+ (get-param intcode (+ 1 offset) mode1)
-                      (get-param intcode (+ 2 offset) mode2))]
-           (recur (assoc intcode (get intcode (+ 3 offset)) res)
-                  (+ 4 offset)
-                  input
-                  output))
-       2 (let [res (* (get-param intcode (+ 1 offset) mode1)
-                      (get-param intcode (+ 2 offset) mode2))]
-           (recur (assoc intcode (get intcode (+ 3 offset)) res)
-                  (+ 4 offset)
-                  input
-                  output))
-       3 (recur (assoc intcode (get intcode (+ 1 offset)) (peek input))
-                (+ 2 offset)
-                (pop input)
-                output)
-       4 (recur intcode
-                (+ 2 offset)
-                input
-                (cons (get-param intcode (+ 1 offset) mode1) output))
-       5 (recur intcode
-                (if (zero? (get-param intcode (+ 1 offset) mode1))
-                  (+ 3 offset)
-                  (get-param intcode (+ 2 offset) mode2))
-                input
-                output)
-       6 (recur intcode
-                (if (zero? (get-param intcode (+ 1 offset) mode1))
-                  (get-param intcode (+ 2 offset) mode2)
-                  (+ 3 offset))
-                input
-                output)
-       7 (recur (if (< (get-param intcode (+ 1 offset) mode1)
-                       (get-param intcode (+ 2 offset) mode2))
-                  (assoc intcode (get intcode (+ 3 offset)) 1)
-                  (assoc intcode (get intcode (+ 3 offset)) 0))
-                (+ 4 offset)
-                input
-                output)
-       8 (recur (if (= (get-param intcode (+ 1 offset) mode1)
-                       (get-param intcode (+ 2 offset) mode2))
-                  (assoc intcode (get intcode (+ 3 offset)) 1)
-                  (assoc intcode (get intcode (+ 3 offset)) 0))
-                (+ 4 offset)
-                input
-                output)
-       99 {:intcode intcode
-           :offset offset
-           :input input
-           :output (reverse output)}))))
+(defn advance-intcode-state
+  "Given an intcode machine, a current offset, and input and output
+  stacks, advances the machine one step, returning the new intcode,
+  offset, input stack, and output stack."
+  [intcode offset input output]
+  (let [[op mode1 mode2 mode3] (split-opcode (get intcode offset))]
+    (case op
+      1 (let [res (+ (get-param intcode (+ 1 offset) mode1)
+                     (get-param intcode (+ 2 offset) mode2))]
+          [(assoc intcode (get intcode (+ 3 offset)) res)
+           (+ 4 offset)
+           input
+           output])
+      2 (let [res (* (get-param intcode (+ 1 offset) mode1)
+                     (get-param intcode (+ 2 offset) mode2))]
+          [(assoc intcode (get intcode (+ 3 offset)) res)
+           (+ 4 offset)
+           input
+           output])
+      3 (if (empty? input)
+          [intcode offset input output]
+          [(assoc intcode (get intcode (+ 1 offset)) (peek input))
+           (+ 2 offset)
+           (pop input)
+           output])
+      4 [intcode
+         (+ 2 offset)
+         input
+         (conj output (get-param intcode (+ 1 offset) mode1))]
+      5 [intcode
+         (if (zero? (get-param intcode (+ 1 offset) mode1))
+           (+ 3 offset)
+           (get-param intcode (+ 2 offset) mode2))
+         input
+         output]
+      6 [intcode
+         (if (zero? (get-param intcode (+ 1 offset) mode1))
+           (get-param intcode (+ 2 offset) mode2)
+           (+ 3 offset))
+         input
+         output]
+      7 [(if (< (get-param intcode (+ 1 offset) mode1)
+                (get-param intcode (+ 2 offset) mode2))
+           (assoc intcode (get intcode (+ 3 offset)) 1)
+           (assoc intcode (get intcode (+ 3 offset)) 0))
+         (+ 4 offset)
+         input
+         output]
+       8 [(if (= (get-param intcode (+ 1 offset) mode1)
+                 (get-param intcode (+ 2 offset) mode2))
+            (assoc intcode (get intcode (+ 3 offset)) 1)
+            (assoc intcode (get intcode (+ 3 offset)) 0))
+          (+ 4 offset)
+          input
+          output]
+       99 [intcode offset input output])))
+
+(defn advance-connected-intcodes
+  "Advance several intcode states in parallel, and then transfer the
+  output of each machine into the input of others as indicated."
+  ([states] (advance-connected-intcodes states {}))
+  ([states connections]
+   (let [computed-states (mapv #(apply advance-intcode-state %) states)]
+     (reduce (fn [sts [out-pos in-pos]]
+               (-> sts
+                   (update-in [in-pos 2]
+                              #(into (get (nth sts out-pos) 3) (reverse %)))
+                   (assoc-in [out-pos 3] '())))
+             computed-states
+             connections))))
+
+;; recurrently advance until the states stop changing
+
+(defn process-connected-intcodes
+  ([states] (process-connected-intcodes states {}))
+  ([states connections]
+   (let [new-states (advance-connected-intcodes states connections)]
+     (if (= states new-states)
+       ;; Get the last machine's output, which may have been sent
+       ;; to some other machine's input
+       (if-let [i (connections (dec (count states)))]
+         (last (get (nth states i) 2))
+         (last (get (last states) 3)))
+       (recur new-states connections)))))
 
 (defn run-amplifiers
-  [intcode phases]
-  (reduce #(:output (process-intcode intcode (conj %1 %2)))
-          '(0)
-          phases))
+  [intcode phases connections]
+  (process-connected-intcodes
+   [[intcode 0 (list (nth phases 0) 0) '()]
+    [intcode 0 (list (nth phases 1)) '()]
+    [intcode 0 (list (nth phases 2)) '()]
+    [intcode 0 (list (nth phases 3)) '()]
+    [intcode 0 (list (nth phases 4)) '()]]
+   connections))
 
 (defn max-output
   "Find the max possible output for an intcode program"
-  [intcode]
-  (let [phase-range (set (range 5))]
-    (->> (for [x1 phase-range
-               x2 (set/difference phase-range #{x1})
-               x3 (set/difference phase-range #{x1 x2})
-               x4 (set/difference phase-range #{x1 x2 x3})
-               x5 (set/difference phase-range #{x1 x2 x3 x4})]
-           (list x1 x2 x3 x4 x5))
-         (map #(first (run-amplifiers intcode %)))
-         (apply max))))
+  [intcode possible-phases connections]
+  (->> (for [x1 possible-phases
+             x2 (set/difference possible-phases #{x1})
+             x3 (set/difference possible-phases #{x1 x2})
+             x4 (set/difference possible-phases #{x1 x2 x3})
+             x5 (set/difference possible-phases #{x1 x2 x3 x4})]
+         (list x1 x2 x3 x4 x5))
+       (map #(run-amplifiers intcode % connections))
+       (apply max)))
 
 (defn read-intcode-file
   "Read input file of an intcode"
@@ -110,6 +140,15 @@
 (defn answer-part-1
   []
   (-> (read-intcode-file "resources/input/day07.txt")
-      (max-output)))
+      (max-output (set (range 5)) {0 1, 1 2, 2 3, 3 4})))
 
-(answer-part-1)
+;; (answer-part-1)
+
+;; Question 2
+
+(defn answer-part-2
+  []
+  (-> (read-intcode-file "resources/input/day07.txt")
+      (max-output (set (range 5 10)) {0 1, 1 2, 2 3, 3 4, 4 0})))
+
+(answer-part-2)
--- test/advent_of_code_2019/day07_test.clj
+++ test/advent_of_code_2019/day07_test.clj
@@ -3,6 +3,32 @@
 (require '[clojure.test :refer :all])
 (require '[advent-of-code-2019.day07 :refer :all])
 
+;; re-testing things from day02/day05 after refactor
+
+(deftest test-advance-intcode-state
+  (testing "it advances intcode state properly"
+    (is (= (apply advance-intcode-state
+                  [[1 0 0 0 99] 0 '() '()])
+           [[2 0 0 0 99] 4 '() '()]))))
+
+(deftest test-advance-connected-intcodes
+  (testing "it advances connected intcodes' states properly"
+    (is (= (advance-connected-intcodes [[[1 0 0 0 99] 0 '() '()]
+                                        [[3 0 4 0 99] 0 '(123) '()]])
+           [[[2 0 0 0 99] 4 '() '()]
+            [[123 0 4 0 99] 2 '() '()]]))))
+
+(deftest test-process-connected-intcodes
+  (testing "it runs intcodes to completion and returns the last output"
+    (is (= (process-connected-intcodes [[[3 0 4 0 99] 0 '(123) '()]
+                                        [[3 0 4 0 99] 0 '() '()]]
+                                       {0 1})
+           123))
+    (is (= (process-connected-intcodes [[[3 0 4 0 99] 0 '(123) '()]
+                                        [[3 0 4 0 99] 0 '() '()]]
+                                       {})
+           nil))))
+
 (def machine1
   [3 15 3 16 1002 16 10 16 1 16 15 15 4 15 99 0 0])
 
@@ -14,16 +40,35 @@
 
 (deftest test-amplifiers
   (testing "it produces the right output for phase settings"
-    (is (= (run-amplifiers machine1 '(4 3 2 1 0))
-           '(43210)))
-    (is (= (run-amplifiers machine2 '(0 1 2 3 4))
-           '(54321)))
-    (is (= (run-amplifiers machine3 '(1 0 4 3 2))
-           '(65210)))))
+    (is (= (run-amplifiers machine1 '(4 3 2 1 0) {0 1, 1 2, 2 3, 3 4})
+           43210))
+    (is (= (run-amplifiers machine2 '(0 1 2 3 4) {0 1, 1 2, 2 3, 3 4})
+           54321))
+    (is (= (run-amplifiers machine3 '(1 0 4 3 2) {0 1, 1 2, 2 3, 3 4})
+           65210))))
 
-(deftest test-max-output
-  (testing "it finds the correct max output over possible phase settings"
-    (is (= (max-output machine1) 43210))
-    (is (= (max-output machine2) 54321))
-    (is (= (max-output machine3) 65210))))
+(deftest test-max-output-1
+  (testing "it finds the correct max output over possible phase settings for question 1"
+    (is (= (max-output machine1 (set (range 5)) {0 1, 1 2, 2 3, 3 4})
+           43210))
+    (is (= (max-output machine2 (set (range 5)) {0 1, 1 2, 2 3, 3 4})
+           54321))
+    (is (= (max-output machine3 (set (range 5)) {0 1, 1 2, 2 3, 3 4})
+           65210))))
+
+(def machine4
+  [3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26
+   27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5])
+
+(def machine5
+  [3 52 1001 52 -5 52 3 53 1 52 56 54 1007 54 5 55 1005 55 26 1001 54
+   -5 54 1105 1 12 1 53 54 53 1008 54 0 55 1001 55 1 55 2 53 55 53 4
+   53 1001 56 -1 56 1005 56 6 99 0 0 0 0 10])
+
+(deftest test-max-output-2
+  (testing "it finds the correct max output over possible phase settings for question 2, with recursive hookups"
+    (is (= (max-output machine4 (set (range 5 10)) {0 1, 1 2, 2 3, 3 4, 4 0})
+           139629729))
+    (is (= (max-output machine5 (set (range 5 10)) {0 1, 1 2, 2 3, 3 4, 4 0})
+           18216))))
 
