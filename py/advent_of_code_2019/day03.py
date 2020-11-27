bac7eec3893409138673852ebc1364519e546c01
Andrew Lim <andrew.b.lim@gmail.com>
2020-11-27 16:56:41 -0500

    Remainder of day03 (py)

--- py/advent_of_code_2019/day03.py
+++ py/advent_of_code_2019/day03.py
@@ -7,13 +7,12 @@ def manhattan(p):
 
 class WireSegment:
 
-    def __init__(self, start, direction, steps, prior_steps):
+    def __init__(self, start, direction, steps):
         assert direction in ["U", "D", "L", "R"]
         assert steps > 0
         self._start = start
         self._direction = direction
         self._steps = steps
-        self._prior_steps = prior_steps
     
     def __repr__(self):
         return f"<WireSegment {self.start} {self.direction}{self.steps}"
@@ -35,8 +34,8 @@ class WireSegment:
         return self._prior_steps
 
     @classmethod
-    def from_desc(cls, start, desc, prior_steps):
-        return cls(start=start, direction=desc[0], steps=int(desc[1:]), prior_steps=prior_steps)
+    def from_desc(cls, start, desc):
+        return cls(start=start, direction=desc[0], steps=int(desc[1:]))
 
     @property
     def is_horizontal(self):
@@ -47,92 +46,101 @@ class WireSegment:
         return self.direction in ["U", "D"]
 
     @property
-    def first_point(self):
+    def points(self):
         if self.direction == "U":
-            return (self.start[0], self.start[1] + 1)
+            return ((self.start[0], self.start[1] + x, self.start[2] + x)
+                    for x in range(1, self.steps + 1))
         elif self.direction == "D":
-            return (self.start[0], self.start[1] - 1)
+            return ((self.start[0], self.start[1] - x, self.start[2] + x)
+                    for x in range(1, self.steps + 1))
         elif self.direction == "L":
-            return (self.start[0] - 1, self.start[1])
+            return ((self.start[0] - x, self.start[1], self.start[2] + x)
+                    for x in range(1, self.steps + 1))
         elif self.direction == "R":
-            return (self.start[0] + 1, self.start[1])
+            return ((self.start[0] + x, self.start[1], self.start[2] + x)
+                    for x in range(1, self.steps + 1))
 
     @property
     def last_point(self):
         if self.direction == "U":
-            return (self.start[0], self.start[1] + self.steps)
+            return (self.start[0], self.start[1] + self.steps, self.start[2] + self.steps)
         elif self.direction == "D":
-            return (self.start[0], self.start[1] - self.steps)
+            return (self.start[0], self.start[1] - self.steps, self.start[2] + self.steps)
         elif self.direction == "L":
-            return (self.start[0] - self.steps, self.start[1])
+            return (self.start[0] - self.steps, self.start[1], self.start[2] + self.steps)
         elif self.direction == "R":
-            return (self.start[0] + self.steps, self.start[1])
+            return (self.start[0] + self.steps, self.start[1], self.start[2] + self.steps)
 
-    @property
-    def minmax_points(self):
-        return sorted([self.first_point, self.last_point])
-
-    def intersection_closest_to_origin(self, other):
-        p1, p2 = self.minmax_points
-        q1, q2 = other.minmax_points
-        if self.is_horizontal and other.is_horizontal and p1[1] == q1[1]:
-            if p1 < q1:
-                start_x = max(p2[0], q1[0])
-                end_x = min(p2[0], q2[0])
-                if end_x >= start_x:
-                    return (min(range(start_x, end_x + 1), key=abs), p1[1])
-            else:
-                start_x = max(q2[0], p1[0])
-                end_x = min(q2[0], p2[0])
-                if end_x >= start_x:
-                    return (min(range(start_x, end_x + 1), key=abs), p1[1])
-        elif self.is_vertical and other.is_vertical and p1[0] == q1[0]:
-            if p1 < q1:
-                start_y = max(p2[1], q1[1])
-                end_y = min(p2[1], q2[1])
-                if end_y >= start_y:
-                    return (p1[0], min(range(start_y, end_y + 1), key=abs))
-            else:
-                start_y = max(q2[1], p1[1])
-                end_y = min(q2[1], p2[1])
-                if end_y >= start_y:
-                    return (p1[0], min(range(start_y, end_y + 1), key=abs))
-        elif self.is_horizontal and other.is_vertical and \
-                p1[0] <= q1[0] <= p2[0] and q1[1] <= p1[1] <= q2[1]:
-            return (q1[0], p1[1])
-        elif self.is_vertical and other.is_horizontal and \
-                p1[1] <= q1[1] <= p2[1] and q1[0] <= p1[0] <= q2[0]:
-            return (p1[0], q1[1])
+    def intersections(self, other):
+        isect = set([])
+        # short-circuit clear non-intersections
+        # could be more efficient but this isn't too bad to read/follow
+        if self.is_horizontal and other.is_horizontal and self.start[1] != other.start[1]:
+            return isect
+        if self.is_vertical and other.is_vertical and self.start[0] != other.start[0]:
+            return isect
+        if self.is_horizontal and other.is_vertical and \
+                ((self.start[0] < other.start[0] and self.last_point[0] < other.start[0]) or
+                 (self.start[0] > other.start[0] and self.last_point[0] > other.start[0]) or
+                 (other.start[1] < self.start[1] and other.last_point[1] < other.start[1]) or
+                 (other.start[1] > self.start[1] and other.last_point[1] > other.start[1])):
+            return isect
+        if self.is_vertical and other.is_horizontal and \
+                ((self.start[1] < other.start[1] and self.last_point[1] < other.start[1]) or
+                 (self.start[1] > other.start[1] and self.last_point[1] > other.start[1]) or
+                 (other.start[0] < self.start[0] and other.last_point[0] < other.start[0]) or
+                 (other.start[0] > self.start[0] and other.last_point[0] > other.start[0])):
+            return isect
+        points = { (p[0], p[1]): p[2] for p in self.points }
+        for q in other.points:
+            if (q[0], q[1]) in points:
+                isect.add((q[0], q[1], q[2] + points[(q[0], q[1])]))
+        return isect
 
 
 def wire_from_desc(start, wire_desc):
     wire = []
     pos = start
-    cumulative_steps = 0
     for wire_seg_desc in wire_desc.split(","):
-        wire_seg = WireSegment.from_desc(pos, wire_seg_desc, cumulative_steps)
+        wire_seg = WireSegment.from_desc(pos, wire_seg_desc)
         wire.append(wire_seg)
         pos = wire_seg.last_point
-        cumulative_steps += wire_seg.steps
     return wire
 
 
 def intersection_closest_to_origin(wire1, wire2):
-    closest = None
+    best = None
     for wire_seg1 in wire1:
         for wire_seg2 in wire2:
-            intersection = wire_seg1.intersection_closest_to_origin(wire_seg2)
-            if intersection is not None:
-                if closest is None or manhattan(closest) > manhattan(intersection):
-                    closest = intersection
-    return closest
+            isect = wire_seg1.intersections(wire_seg2)
+            if len(isect) > 0:
+                candidate = min(isect, key=manhattan)
+                if best is None or manhattan(best) > manhattan(candidate):
+                    best = candidate
+    return best
+
+
+def intersection_fewest_steps(wire1, wire2):
+    best = None
+    for wire_seg1 in wire1:
+        for wire_seg2 in wire2:
+            isect = wire_seg1.intersections(wire_seg2)
+            if len(isect) > 0:
+                candidate = min(isect, key=lambda x: x[2])
+                if best is None or best[2] > candidate[2]:
+                    best = candidate
+    return best
 
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as fp:
-        wire1 = wire_from_desc((0,0), fp.readline().rstrip())
-        wire2 = wire_from_desc((0,0), fp.readline().rstrip())
+        wire1 = wire_from_desc((0,0,0), fp.readline().rstrip())
+        wire2 = wire_from_desc((0,0,0), fp.readline().rstrip())
     print("Part 1:")
     isect = intersection_closest_to_origin(wire1, wire2)
     print(isect)
     print(manhattan(isect))
+    print("Part 2:")
+    isect = intersection_fewest_steps(wire1, wire2)
+    print(isect)
+    print(isect[2])
--- py/tests/test_day03.py
+++ py/tests/test_day03.py
@@ -4,17 +4,34 @@ from advent_of_code_2019.day03 import *
 
 
 def test_intersection_closest():
-    wire1 = wire_from_desc((0,0), "R8,U5,L5,D3")
-    wire2 = wire_from_desc((0,0), "U7,R6,D4,L4")
+    wire1 = wire_from_desc((0,0,0), "R8,U5,L5,D3")
+    wire2 = wire_from_desc((0,0,0), "U7,R6,D4,L4")
     isect = intersection_closest_to_origin(wire1, wire2)
-    assert isect == (3,3)
+    assert (isect[0], isect[1]) == (3,3)
 
-    wire1 = wire_from_desc((0,0), "R75,D30,R83,U83,L12,D49,R71,U7,L72")
-    wire2 = wire_from_desc((0,0), "U62,R66,U55,R34,D71,R55,D58,R83")
+    wire1 = wire_from_desc((0,0,0), "R75,D30,R83,U83,L12,D49,R71,U7,L72")
+    wire2 = wire_from_desc((0,0,0), "U62,R66,U55,R34,D71,R55,D58,R83")
     isect = intersection_closest_to_origin(wire1, wire2)
     assert manhattan(isect) == 159
     
-    wire1 = wire_from_desc((0,0), "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
-    wire2 = wire_from_desc((0,0), "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+    wire1 = wire_from_desc((0,0,0), "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
+    wire2 = wire_from_desc((0,0,0), "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
     isect = intersection_closest_to_origin(wire1, wire2)
     assert manhattan(isect) == 135
+
+
+def test_intersection_fewest_steps():
+    wire1 = wire_from_desc((0,0,0), "R8,U5,L5,D3")
+    wire2 = wire_from_desc((0,0,0), "U7,R6,D4,L4")
+    isect = intersection_fewest_steps(wire1, wire2)
+    assert isect == (6,5,30)
+
+    wire1 = wire_from_desc((0,0,0), "R75,D30,R83,U83,L12,D49,R71,U7,L72")
+    wire2 = wire_from_desc((0,0,0), "U62,R66,U55,R34,D71,R55,D58,R83")
+    isect = intersection_fewest_steps(wire1, wire2)
+    assert isect[2] == 610
+
+    wire1 = wire_from_desc((0,0,0), "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51")
+    wire2 = wire_from_desc((0,0,0), "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+    isect = intersection_fewest_steps(wire1, wire2)
+    assert isect[2] == 410
